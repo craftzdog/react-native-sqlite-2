dd580ee01adf9b37bad3b5ae257c2b75b2594030
Takuya Matsuyama <hi@craftz.dog>
2019-09-11 15:42:41 +0900

    chore(ios): change flag name for debug logs

--- ios/RNSqlite2.h
+++ ios/RNSqlite2.h
@@ -7,7 +7,7 @@
 
 #import <Foundation/Foundation.h>
 
-#ifdef shouldLog
+#ifdef DEBUG_RNSqlite2
 #define logDebug(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
 #else
 #define logDebug(...)
