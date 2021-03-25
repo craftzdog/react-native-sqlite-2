4295d3bc5a96e9dc22a1634c6bef8bc7838697fc
Takuya Matsuyama <hi@craftz.dog>
2021-03-25 18:24:28 +0900

    chore(dev): update dev deps

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import module from './src'
 
 if (!process.nextTick) {
   process.nextTick = function (callback) {
-    setTimeout(callback, 0);
-  };
+    setTimeout(callback, 0)
+  }
 }
 
-export default module;
+export default module
